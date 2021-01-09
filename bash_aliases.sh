2d76c3921d85000b5ceb7e8150e16fba78cf1b04
masterdoed <masterdoed@doedspace.de>
2021-01-09 22:37:50 +0100

    bash aliases added

---
+++ bash_aliases.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+alias ll="/bin/ls -lsa"
+alias parts="/bin/df -ha"
+alias space="/bin/du -ha"
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias sha1='openssl sha1'
+alias diff='colordiff'
+alias h='history'
+alias j='jobs -l'
+alias ping='ping -c 5'
+alias fastping='ping -c 100 -s.2'
+alias ports='netstat -tulanp'
+alias wget='wget -c'
